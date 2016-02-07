8092839c0ef8ae034cb9bd5ecff08ca5c8063fbd
Stoian <stoian.e.stoianov@gmail.com>
2016-02-07 11:31:43 +0200

    Changes in login form

--- BooksLibrary.Web/App_Start/IdentityConfig.cs
+++ BooksLibrary.Web/App_Start/IdentityConfig.cs
@@ -45,17 +45,17 @@ namespace BooksLibrary.Web
             manager.UserValidator = new UserValidator<User>(manager)
             {
                 AllowOnlyAlphanumericUserNames = false,
-                RequireUniqueEmail = true
+                RequireUniqueEmail = false
             };
 
             // Configure validation logic for passwords
             manager.PasswordValidator = new PasswordValidator
             {
-                RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
-                RequireDigit = true,
-                RequireLowercase = true,
-                RequireUppercase = true,
+                RequiredLength = 3,
+                RequireNonLetterOrDigit = false,
+                RequireDigit = false,
+                RequireLowercase = false,
+                RequireUppercase = false,
             };
 
             // Register two factor authentication providers. This application uses Phone and Emails as a step of receiving a code for verifying the user
